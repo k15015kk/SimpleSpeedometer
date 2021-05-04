1fa1e0232b897b95e9dcac736a342031c1252f82
k15015kk <b19702bb@aitech.ac.jp>
2021-05-04 21:11:15 +0900

    Initial Commit

---
+++ SimpleSpeedometer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleSpeedometer
+//
+//  Created by 井上晴稀 on 2021/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
