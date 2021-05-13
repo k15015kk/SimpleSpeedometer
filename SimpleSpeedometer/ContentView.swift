c5bdfd35096b7117f9e2e5ad98241d2dad6c6d49
k15015kk <b19702bb@aitech.ac.jp>
2021-05-14 01:47:56 +0900

    地図の追従モードを追加

--- SimpleSpeedometer/ContentView.swift
+++ SimpleSpeedometer/ContentView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
-    @ObservedObject var lh = LocationHelper(start: true)
     let font = Font.custom("HiraginoSans-W3",size: 18)
+    @State var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 35.681111, longitude: 139.766667),
+            latitudinalMeters: 300,longitudinalMeters: 300)
+    // 速度を表示するためにLocationHelperクラスを宣言
+    @ObservedObject var lh = LocationHelper(start: true)
     
     var body: some View {
         VStack(alignment: .center, spacing: 8){
@@ -39,6 +44,9 @@ struct ContentView: View {
                     .font(.custom("Futura", size: 24))
             }.padding(.vertical, 8)
             Spacer()
+            HStack {
+                Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, userTrackingMode: .constant(.follow))
+            }
         }
         .padding(.horizontal ,16)
     }
